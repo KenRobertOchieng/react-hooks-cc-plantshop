1b09d85d766138958e33cca2fe6d57687e749070
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-04-25 17:02:01 +0300

    change

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  function handleToggleStock(id) {
+    const updated = plants.map((p) =>
+      p.id === id ? { ...p, inStock: !p.inStock } : p
+    );
+    setPlants(updated);
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={displayedPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
